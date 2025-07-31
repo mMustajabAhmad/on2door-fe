544c898814df9dc22bcf67f98ed190a0f768adf6
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-31 10:56:14 +0500

    strucutre: add view to show admin

---
+++ src/views/on2door/apps/administrators/admins/view/admin-left-overview/index.jsx
@@ -0,0 +1,21 @@
+// MUI Imports
+import Grid from '@mui/material/Grid2'
+
+// Component Imports
+import UserDetails from './AdminDetails'
+import UserPlan from './AdminPlan'
+
+const UserLeftOverview = ({ userData }) => {
+  return (
+    <Grid container spacing={6}>
+      <Grid size={{ xs: 12 }}>
+        <UserDetails userData={userData} />
+      </Grid>
+      <Grid size={{ xs: 12 }}>
+        <UserPlan userData={userData} />
+      </Grid>
+    </Grid>
+  )
+}
+
+export default UserLeftOverview
