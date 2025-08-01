76abe94ebca403fff8837f6f2dd11eb941f2c75d
ayemenn <aymenmehmood812@gmail.com>
2025-08-01 15:30:05 +0500

    authentication: add signin with google option

--- src/views/on2door/auth/Signin.jsx
+++ src/views/on2door/auth/Signin.jsx
@@ -243,6 +243,15 @@ const Login = ({ mode }) => {
           </form>
 
           <Divider className='gap-3'>or</Divider>
+          <Button
+            color='secondary'
+            className='self-center text-textPrimary'
+            startIcon={<img src='/images/logos/google.png' alt='Google' width={22} />}
+            sx={{ '& .MuiButton-startIcon': { marginInlineEnd: 3 } }}
+            onClick={() => signIn('google')}
+          >
+            Sign in with Google
+          </Button>
         </div>
       </div>
     </div>
