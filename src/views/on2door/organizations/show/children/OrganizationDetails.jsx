98355f4c9975fe823ed9ed1b88a156a98c5e7fad
mMustajabAhmad <mustajabajwah@gmail.com>
2025-09-03 16:57:45 +0500

    organization: add details page

---
+++ src/views/on2door/organizations/show/children/OrganizationDetails.jsx
@@ -0,0 +1,105 @@
+// MUI Imports
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
+import Button from '@mui/material/Button'
+
+// Component Imports
+import OpenDialogOnElementClick from '@/components/on2door/dialogs/OpenDialogOnElementClick'
+import EditOrganizationDialog from '@/components/on2door/dialogs/organization/update'
+import CustomAvatar from '@core/components/mui/Avatar'
+
+// Util Imports
+import { getInitials } from '@/utils/getInitials'
+
+const OrganizationDetails = ({ organizationData }) => {
+  const organization = organizationData.organization?.data?.attributes || {}
+  const organizationName = organization.name || 'N/A'
+
+  return (
+    <>
+      <Card>
+        <CardContent className='flex flex-col pbs-12 gap-6'>
+          <div className='flex flex-col gap-6'>
+            <div className='flex items-center justify-center flex-col gap-4'>
+              <div className='flex flex-col items-center gap-4'>
+                <CustomAvatar alt='organization-profile' variant='rounded' size={120} skin='light'>
+                  {getInitials(organizationName)}
+                </CustomAvatar>
+                <Typography variant='h5'>{organizationName}</Typography>
+              </div>
+              <div className='flex items-center flex-wrap gap-x-1.5'>
+                <Typography>{organization.message || 'N/A'}</Typography>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <Typography variant='h5'>Organization Details</Typography>
+            <Divider className='mlb-4' />
+            <div className='flex flex-col gap-3'>
+              <div className='flex items-center flex-wrap gap-x-1.5'>
+                <Typography className='font-medium' color='text.primary'>
+                  Name:
+                </Typography>
+                <Typography>{organization.name || 'N/A'}</Typography>
+              </div>
+
+              <div className='flex items-center flex-wrap gap-x-1.5'>
+                <Typography className='font-medium' color='text.primary'>
+                  Email:
+                </Typography>
+                <Typography>{organization.email || 'N/A'}</Typography>
+              </div>
+
+              <div className='flex items-center flex-wrap gap-x-1.5'>
+                <Typography className='font-medium' color='text.primary'>
+                  Industry:
+                </Typography>
+                <Typography>{organization.primary_industry || 'N/A'}</Typography>
+              </div>
+
+              <div className='flex items-center flex-wrap gap-x-1.5'>
+                <Typography className='font-medium' color='text.primary'>
+                  Monthly Delivery Voulme:
+                </Typography>
+                <Typography>{organization.monthly_delivery_volume || 'N/A'}</Typography>
+              </div>
+
+              <div className='flex items-center flex-wrap gap-x-1.5'>
+                <Typography className='font-medium' color='text.primary'>
+                  Country:
+                </Typography>
+                <Typography>{organization.country || 'N/A'}</Typography>
+              </div>
+
+              <div className='flex items-center flex-wrap gap-x-1.5'>
+                <Typography className='font-medium' color='text.primary'>
+                  TimeZone:
+                </Typography>
+                <Typography>{organization.timezone|| 'N/A'}</Typography>
+              </div>
+            </div>
+          </div>
+
+          <div className='flex gap-4 justify-center'>
+            <OpenDialogOnElementClick
+              element={Button}
+              elementProps={{ children: 'Edit organization', variant: 'contained', color: 'primary' }}
+              dialog={EditOrganizationDialog}
+              dialogProps={{
+                data: organizationData,
+                currentOrganization: {
+                  id: organization.id,
+                }
+              }}
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  )
+}
+
+export default OrganizationDetails
